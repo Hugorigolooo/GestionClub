7e6e89a1a80fb8f8e68a8eaf405bbc16aaedf1dd
h.valentin <hugovtn2004@gmail.com>
2024-04-17 13:54:07 -1000

    Mise à jour dashboard.blade.php

--- GestionClub/resources/views/dashboard.blade.php
+++ GestionClub/resources/views/dashboard.blade.php
@@ -1,68 +1,24 @@
+@extends("layout.master")
+@section('content')
+    <x-app-layout>
+        <x-slot name="header">
+            <h2 class="font-semi-bold text-xl text-gray-800 leading-tight">
+                {{ __('Dashboard') }}
+            </h2>
+        </x-slot>
 
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            Gestion des utilisateurs
-        </h2>
-    </x-slot>
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <h3 class="text-3xl mx-4 my-4">Les utilisateurs du site</h3>
-                <div class="flex flex-col">
-                    <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-                        <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-                            <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                                <table class="min-w-full divide-y divide-gray-200">
-                                    <thead class="bg-gray-50">
-                                    <tr>
-                                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500
-uppercase tracking-wider">
-                                            Nom
-                                        </th>
-                                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500
-uppercase tracking-wider">
-                                            Email
-                                        </th>
-                                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500
-uppercase tracking-wider">
-                                            Role
-                                        </th>
-                                        <th scope="col" class="relative px-6 py-3">
-                                            <span class="sr-only">Modifier</span>
-                                        </th>
-                                    </tr>
-                                    <tbody class="bg-white divide-y divide-gray-200">
-                                    @foreach($users as $user)
-                                        <tr>
-                                            <td class="px-6 py-4 whitespace-nowrap">
-                                                <div class="text-sm font-medium text-gray-900">
-                                                    {{ $user->name }}
-                                                </div>
-                                            </td>
-                                            <td class="px-6 py-4 whitespace-nowrap">
-                                                <div class="text-sm font-medium text-gray-900">
-                                                    {{ $user->email }}
-                                                </div>
-                                            </td>
-                                            <td class="px-6 py-4 whitespace-nowrap">
-                                                <div class="text-sm font-medium text-gray-900">
-                                                    {{ $user->admin ? 'Administrateur' : 'Utilisateur' }}
-                                                </div>
-                                            </td>
-                                            <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                                                <a href="{{ route('users.edit', $user) }}" class="text-indigo-600
-hover:text-indigo-900">Modifier</a>
-                                            </td>
-                                        </tr>
-                                    @endforeach
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900">
+                        {{ __("You're logged in!") }}
+                        <a href="{{ route('users') }}" class="inline-flex items-center px-6 py4 border border-gray-400 shadow-sm text-base font-medium rounded-md textgray-700 bg-white">
+                            Gestion des utilisateurs
+                        </a>
+
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-</x-app-layout>
+    </x-app-layout>
+@endsection
