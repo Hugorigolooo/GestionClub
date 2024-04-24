e0f7f2f8b6f8d344d60918f99059df669469f09b
h.valentin <hugovtn2004@gmail.com>
2024-04-24 13:37:40 -1000

    Mise à jour dashboard.blade.php

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '7e6e89a1a80fb8f8e68a8eaf405bbc16aaedf1dd',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '7e6e89a1a80fb8f8e68a8eaf405bbc16aaedf1dd',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
