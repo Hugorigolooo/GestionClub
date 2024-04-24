db10692900bac195fa7031273bd1835616bd9a23
h.valentin <hugovtn2004@gmail.com>
2024-04-24 13:35:45 -1000

    Mise à jour dashboard.blade.php

--- GestionClub/routes/web.php
+++ GestionClub/routes/web.php
@@ -1,4 +1,86 @@
 <?php
+//
+///*
+//|--------------------------------------------------------------------------
+//| Web Routes
+//|--------------------------------------------------------------------------
+//|
+//| Here is where you can register web routes for your application. These
+//| routes are loaded by the RouteServiceProvider and all of them will
+//| be assigned to the "web" middleware group. Make something great!
+//|
+//*/
+//
+////use App\Http\Controllers\ProfileController;
+//use App\Http\Controllers\UsersController;
+//use Illuminate\Support\Facades\Route;
+////use Symfony\Component\Routing\Annotation\Route;
+//
+//Route::get('/', function () {
+//    return view('/bienvenue');
+//});
+//
+//Route::get('/dashboard', function () {
+//    return view('/dashboard');
+//})->middleware(['auth', 'verified'])->name('dashboard');
+//
+//Route::get('/apropos', function (){
+//    return view('/pages/apropos');
+//});
+//
+//Route::get('/contact', function (){
+//    return view('/pages/contact');
+//});
+//
+//Route::get('/auth/login', function () {
+//    return view('/login');
+//});
+//
+//Route::get('/auth/register', function () {
+//    return view('/register');
+//});
+//
+//
+//
+//Route::middleware('auth')->group(function () {
+//    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+////    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//   Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+//});
+//
+//
+////
+////Route::get('/users', [UsersController::class, 'index'])->middleware(['auth',
+////    'admin'])->name('users');
+////
+////Route::delete('/users/{id}', [UsersController::class,
+////    'destroy'])->middleware(['auth', 'admin'])->name('users.destroy');
+//
+//Route::get('/dashboard', function () { return view('dashboard');} )
+//    ->middleware(['auth'])->name('dashboard');
+//
+//Route::get('/users', [UsersController::class, 'index'])->middleware(['auth',
+//    'admin'])->name('users');
+//
+////Route::get('/users/profile/{user}', [UsersController::class, 'show'])
+// //   ->name('users.show');
+//
+//Route::get('/users/{user}', [UsersController::class, 'edit'])
+//    ->middleware(['auth'])->name('users.edit');
+//
+//Route::put('/users/{user}', [UsersController::class, 'update'])
+//    ->middleware(['auth'])->name('users.update');
+//
+//Route::delete('/users/{user}', [UsersController::class, 'destroy'])
+//    ->middleware(['auth', 'admin'])->name('users.destroy');
+//
+//require __DIR__.'/auth.php';
+//
+
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\IndexController;
 
 /*
 |--------------------------------------------------------------------------
@@ -11,35 +93,23 @@
 |
 */
 
-use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\UsersController;
-use Illuminate\Support\Facades\Route;
-//use Symfony\Component\Routing\Annotation\Route;
+Route::get('/', [IndexController::class, 'index']);
 
-Route::get('/', function () {
+Route::get('/apropos', function () {
+    return view('/pages/apropos');
+});
+
+Route::get('/contact', function () {
+    return view('/pages/contact');
+});
+
+Route::get('/index', function () {
     return view('/bienvenue');
 });
 
 Route::get('/dashboard', function () {
-    return view('/dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::get('/apropos', function (){
-    return view('/pages/apropos');
-});
-
-Route::get('/contact', function (){
-    return view('/pages/contact');
-});
-
-Route::get('/auth/login', function () {
-    return view('/login');
-});
-
-Route::get('/auth/register', function () {
-    return view('/register');
-});
-
+    return view('dashboard');
+})->middleware(['admin'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -47,28 +117,21 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
-//
-//Route::get('/users', [UsersController::class, 'index'])->middleware(['auth',
-//    'admin'])->name('users');
-//
-//Route::delete('/users/{id}', [UsersController::class,
-//    'destroy'])->middleware(['auth', 'admin'])->name('users.destroy');
+Route::get('/users', [\App\Http\Controllers\UsersController::class, 'index'])->middleware(['auth', 'admin'])->name('users');
 
-Route::get('/dashboard', function () { return view('dashboard');} )
-    ->middleware(['auth'])->name('dashboard');
-
-Route::get('/users', [UsersController::class, 'index'])->middleware(['auth',
-    'admin'])->name('users');
-
-Route::get('/users/profile/{user}', [UsersController::class, 'show'])
+Route::get('/users/profile/{user}', [\App\Http\Controllers\UsersController::class, 'show'])
     ->name('users.show');
 
-Route::get('/users/{user}', [UsersController::class, 'edit'])
+Route::get('/users/{user}', [\App\Http\Controllers\UsersController::class, 'edit'])
     ->middleware(['auth'])->name('users.edit');
 
-Route::put('/users/{user}', [UsersController::class, 'update'])
+//Route::get('/users/{user}', [\App\Http\Controllers\UsersController::class, 'edit'])->name('users.edit');
+
+Route::put('/users/{user}', [\App\Http\Controllers\UsersController::class, 'update'])
     ->middleware(['auth'])->name('users.update');
 
-Route::delete('/users/{user}', [UsersController::class, 'destroy'])
-    ->middleware(['auth', 'admin'])->name('users.destroy');
+Route::delete('/users/{user}', [\App\Http\Controllers\UsersController::class, 'destroy'])
+    ->middleware(['auth', 'verified', 'admin'])->name('users.destroy');
+
+
+require __DIR__ . '/auth.php';
